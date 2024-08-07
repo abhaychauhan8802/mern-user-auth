955a5ee8d4454dd70a21e87509861fde8b1267e5
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2024-08-07 13:53:07 +0530

    create a middleware for errors

--- server/index.js
+++ server/index.js
@@ -15,6 +15,18 @@ mongoose
 // middlewares
 app.use(express.json());
 
+// error middlewar
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(8080, () => {
   console.log("Server started..");
 });
