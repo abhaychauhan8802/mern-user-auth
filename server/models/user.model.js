5af3d69d182859f0943e34a69ae97b6836cc4f4e
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2024-08-07 14:02:40 +0530

    add user model

---
+++ server/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      require: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
